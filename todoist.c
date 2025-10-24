66a71af4107d5f7dfcf02df54672ac1ca3d6f0e7
chachacollins <collinschacha@hotmail.com>
2025-10-24 21:25:45 +0300

    feat: can now add todos

--- todoist.c
+++ todoist.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 
 #define NODISCARD __attribute__((warn_unused_result))
 #define UNUSED(var) (void)(var);
-#define EPRINT(fmt, ...) fprintf(stderr, "ERROR:"fmt"\n", __VA_ARGS__)
+#define EPRINT(fmt) fprintf(stderr, "ERROR: " fmt "\n")
+#define EPRINTF(fmt, ...) fprintf(stderr, "ERROR:" fmt "\n", __VA_ARGS__)
+#define HELP(fmt) fprintf(stderr, "USAGE: "fmt"\n")
+#define TODOS_FILE "todos.txt"
 
-typedef NODISCARD int (*comm_fn)(int *argc, char** argv);
+typedef NODISCARD int (*comm_fn)(int *argc, char*** argv);
 
 typedef struct {
     const char* s_flag;
     const char* l_flag;
-    const char* help;
+    const char* desc;
     comm_fn fn;
 } Command;
 
@@ -28,7 +32,7 @@ static int add_command(Commands* comms, Command command)
     assert(comms != NULL);
     if(comms->size >= COMMAND_CAPACITY)
     {
-        EPRINT("Commands added exceed maximum capacity: %d\n", COMMAND_CAPACITY);
+        EPRINTF("Commands added exceed maximum capacity: %d\n", COMMAND_CAPACITY);
         return 0;
     }
     comms->commands[comms->size++] = command;
@@ -36,15 +40,15 @@ static int add_command(Commands* comms, Command command)
 }
 
 NODISCARD
-static const char* shift_args(int *argc, char** argv)
+static const char* shift_args(int *argc, char*** argv)
 {
     if(*argc < 1) return NULL;
     *argc -= 1;
-    return *argv++;
+    return *(*argv)++;
 }
 
 NODISCARD  
-static int usage(int *argc, char** argv)
+static int help_command(int *argc, char*** argv)
 {
     UNUSED(argc);
     UNUSED(argv);
@@ -56,33 +60,73 @@ static int usage(int *argc, char** argv)
         Command command = commands.commands[i];
         assert(command.s_flag != NULL);
         assert(command.l_flag != NULL);
-        assert(command.help != NULL);
-        printf("    %s, %-10s  %s\n", command.s_flag, command.l_flag, command.help);
+        assert(command.desc != NULL);
+        printf("    %s, %-10s  %s\n", command.s_flag, command.l_flag, command.desc);
     }
-    return 1;
+    return 0;
+}
+
+NODISCARD
+static int add_task_command(int *argc, char*** argv)
+{
+    const char* task = shift_args(argc, argv);
+    if(task == NULL)
+    {
+        EPRINT("Please provide a task to be added to the list of todos"); 
+        HELP("td add \"TASK TO BE ADDED\"");
+        return 1;
+    }
+    FILE *todos_file = fopen(TODOS_FILE, "a");
+    if(todos_file == NULL)
+    {
+        perror("ERROR:");
+        return 1;
+    }
+    size_t task_len = strlen(task) + 1;
+    size_t written_bytes = fprintf(todos_file, "%s\n", task);
+    if(task_len != written_bytes)
+    {
+        EPRINTF("Failed to write all bytes of %s, expected: %zu but wrote %zu", task, task_len, written_bytes);
+        fclose(todos_file);
+        return 1;
+    }
+    fclose(todos_file);
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    (void)argv;
+    const char* program_name = shift_args(&argc, &argv);
+    UNUSED(program_name);
     Command help =  {
         "-h", 
         "help", 
         "prints this help message and quits",
-        usage,
+        help_command,
     };
     Command add = {
         "-a",
         "add",
         "adds a todo",
-        NULL,
+        add_task_command,
     };
     if(!add_command(&commands, add)) return 1;
     if(!add_command(&commands, help)) return 1;
-    if(argc < 2)
+    if(argc < 1)
     {
-        if(!usage(&argc, argv)) return 1;
+        if(help_command(&argc, &argv)) return 1;
         return 1;
     }
-    return 0;
+    const char* flag = shift_args(&argc, &argv);
+    for(int i = 0; i < commands.size; ++i)
+    {
+        Command command = commands.commands[i];
+        if(strcmp(command.s_flag, flag) == 0 || strcmp(command.l_flag, flag) == 0)
+        {
+            assert(command.fn != NULL);
+            return command.fn(&argc, &argv);
+        }
+    }
+    EPRINTF("Unrecognized option passed: %s", flag);
+    return 1;
 }
